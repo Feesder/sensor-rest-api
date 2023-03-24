14f3153d0a715c4b229dd9209e4fb311dcdbfa6e
feesder <qop6261@gmail.com>
2023-03-24 22:51:58 +0600

    commit

--- src/main/java/com/project/sensor/exception/DeviceNotFoundException.java
+++ src/main/java/com/project/sensor/exception/DeviceNotFoundException.java
@@ -1,6 +1,7 @@
 package com.project.sensor.exception;
 
 public class DeviceNotFoundException extends Exception {
+
     public DeviceNotFoundException(String message){
         super(message);
     }
