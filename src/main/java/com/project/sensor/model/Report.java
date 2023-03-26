1cd373b648bcce24b927f8fa1b7a339181e6ff2e
feesder <qop6261@gmail.com>
2023-03-26 14:25:42 +0600

    commit

--- src/main/java/com/project/sensor/model/Report.java
+++ src/main/java/com/project/sensor/model/Report.java
@@ -14,26 +14,33 @@ import java.util.List;
 
 public class Report {
     private Long id;
-    private Long deviceId;
+    private Device device;
     private Integer temperature;
     private Integer gas;
     private Integer damp;
     private Timestamp date;
 
-    public Report(Long id, Integer temperature, Integer gas, Integer damp, Timestamp date, Long deviceId) {
+    public Report(Long id, Integer temperature, Integer gas, Integer damp, Timestamp date, Device device) {
         this.id = id;
         this.temperature = temperature;
         this.gas = gas;
         this.damp = damp;
         this.date = date;
-        this.deviceId = deviceId;
+        this.device = device;
     }
 
     public static Report toModel(ReportEntity report) {
         return new Report(
                 report.getId(), report.getTemperature(),
-                report.getGas(), report.getDamp(), report.getDate(), report.getDevice().getId()
+                report.getGas(), report.getDamp(), report.getDate(), deviceToModel(report.getDevice())
         );
+    }
+
+    public static Device deviceToModel(DeviceEntity deviceEntity) {
+        Device device = Device.toModel(deviceEntity);
+        device.setReport(null);
+        device.setUser(null);
+        return device;
     }
 
     public static List<Report> toModel(Iterator<ReportEntity> reports) {
@@ -91,11 +98,4 @@ public class Report {
         this.date = date;
     }
 
-    public Long getDeviceId() {
-        return deviceId;
-    }
-
-    public void setDeviceId(Long deviceId) {
-        this.deviceId = deviceId;
-    }
 }
